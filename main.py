6a9440fe81f7b410e72c1ede35797ce03f41686a
Einsworth <east2589@gmail.com>
2021-11-01 03:38:33 +0700

    work (2)

--- main.py
+++ main.py
@@ -37,29 +37,46 @@ class Deck( list ):
         for value in values:
             for suit in suits:
                 self.append(Card(value, suit))
-                    
-#background setting
-def background():
-    screen.fill((0, 100, 0))
-    pygame.display.update()
+    def deal(self):
+        return self.pop()
 
-#prepare before game start
-def gameSetting():
-    deck = Deck()
-    print(deck)
-    random.shuffle(deck)
-    print(deck)
-    
+class Player( object ):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+    def draw(self, deck):
+        self.hand.append(deck.deal())
+
+def image(x):
+    path =  "pictures/" + str(x) + ".png"
+    return pygame.image.load(path)
+
 #main game function
-def main():
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                pygame.quit()
+#prepare before game start
+running = True
+deck = Deck()
+random.shuffle(deck)
+p1 = Player("Player 1")
+p2 = Player("Player 2")
+p3 = Player("Player 3")
+p4 = Player("Player 4")
+for card in deck:
+    p1.draw(deck)
+    p2.draw(deck)
+    p3.draw(deck)
+    p4.draw(deck)
+print("Player1 = "); print(p1.hand)
+print("Player2 = "); print(p2.hand)
+print("Player3 = "); print(p3.hand)
+print("Player4 = "); print(p4.hand)
 
-        background()
+while running:
+    screen.fill((0, 100, 0))
+    screen.blit(pygame.transform.scale(image(p1.hand[1]), (125, 180)), (0, 0))
 
-gameSetting()
-main()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+    
+    pygame.display.update()
